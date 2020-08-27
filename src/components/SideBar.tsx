b6084ff269581997668b6491983b05026b682241
danilomacb <danilo.bakun@gmail.com>
2020-08-27 10:27:24 -0300

    profile description

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,6 +5,13 @@ import "../styles/sidebar.scss";
 function SideBar() {
   return (
     <nav>
+      <img src={`${process.env.PUBLIC_URL}/images/perfil.png`} alt="Perfil"/>
+      <h1>Danilo Macedo Bakun</h1>
+      <ul id="profileDescription">
+        <li>Desenvolvedor FullStack JavaScript</li>
+        <li>Stack React + Node</li>
+        <li>Usuário de distros Linux baseadas em Ubuntu</li>
+      </ul>
       <ul>
         <li>item 1</li>
         <li>item 2</li>
