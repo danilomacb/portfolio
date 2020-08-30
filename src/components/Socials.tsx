36e3ad8599f79e618bd8d9a9121f765c51c1d10a
danilomacb <danilo.bakun@gmail.com>
2020-08-30 07:38:36 -0300

    using noopener noreferrer on socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -8,19 +8,19 @@ import "../styles/socials.scss";
 function Socials() {
   return (
     <div id="socials">
-      <a href="https://github.com/danilomacb" target="_blank">
+      <a href="https://github.com/danilomacb" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faGithub} size="lg" />
       </a>
-      <a href="https://www.linkedin.com/in/danilomacb" target="_blank">
+      <a href="https://www.linkedin.com/in/danilomacb" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faLinkedin} size="lg" />
       </a>
-      <a href="https://www.facebook.com/danilomacb" target="_blank">
+      <a href="https://www.facebook.com/danilomacb" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faFacebook} size="lg" />
       </a>
-      <a href="https://twitter.com/danilomacb" target="_blank">
+      <a href="https://twitter.com/danilomacb" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faTwitter} size="lg" />
       </a>
-      <a href="mailto:danilo.macb@gmail.com" target="_blank">
+      <a href="mailto:danilo.macb@gmail.com" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faEnvelope} size="lg" />
       </a>
     </div>
