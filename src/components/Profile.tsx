ca40bf0c688d81c2e680d17f362d9edf54cc2f5d
danilomacb <danilo.bakun@gmail.com>
2020-08-31 10:32:32 -0300

    badges on profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 
 import "../styles/profile.scss";
+import badges from "../badges.json";
 
 function Profile() {
   return (
     <div id="profile">
-      <img src={`${process.env.PUBLIC_URL}/images/perfil.png`} alt="Perfil" />
+      <img src={`${process.env.PUBLIC_URL}/images/perfil.png`} alt="Perfil" id="profilePicture" />
       <h1>Danilo Macedo Bakun</h1>
       <ul id="profileDescription">
-        <li>Desenvolvedor FullStack JavaScript</li>
-        <li>Stack React + Node</li>
-        <li>Usuário de distros Linux baseadas em Ubuntu</li>
+        <li>
+          Desenvolvedor FullStack{" "}
+          <img
+            src={badges[2]}
+            alt="JavaScript"
+            className="badge"
+          />
+        </li>
+        <li>
+          Stack{" "}
+          <img src={badges[3]} alt="React" className="badge" /> +{" "}
+          <img src={badges[4]} alt="Node" className="badge" />
+        </li>
+        <li>
+          Usuário de distros{" "}
+          <img src={badges[5]} alt="Linux" className="badge" />{" "}
+          baseadas em{" "}
+          <img src={badges[6]} alt="Ubuntu" className="badge" />
+        </li>
       </ul>
     </div>
   );
