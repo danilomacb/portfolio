f4fd9cb88059362f73b228384ab7cad529a2a106
danilomacb <danilo.bakun@gmail.com>
2020-10-06 09:25:28 -0300

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 
 import "./styles/index.scss";
 import "react-image-lightbox/style.css";
@@ -6,6 +7,11 @@ import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-179778647-1");
+    ReactGA.pageview("/");
+  }, []);
+
   return (
     <>
       <SideBar />
